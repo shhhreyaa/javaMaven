0dea6fab5b4fe5c4e9338e2641ff443e97d6be41
shreya <shreya04kulkarni@gmail.com>
2025-01-02 20:56:36 +0530

    pls god

---
+++ src/main/java/com/JavaMavenEclipse/javamaveneclipse/App.java
@@ -0,0 +1,13 @@
+package com.JavaMavenEclipse.javamaveneclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
